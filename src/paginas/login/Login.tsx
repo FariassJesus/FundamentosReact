bba94418aa8b449d54e870e14e2ff8ad816e0d84
Jesus <je-farias@hotmail.com>
2022-09-14 00:25:11 -0300

    [Fundamentos React Task: 3] {14/09/2022}

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,24 +1,72 @@
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Button, Grid, TextField, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
 function Login () {
+
+    let navigate = useNavigate();
+// irá guardar/controlar o uso do token na API
+    const [token, setToken] = useLocalStorage('token');
+
+// UserLogin acessa a informação do do useState
+//setUserLogin altera a informação do useState
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {//fazendo UserLogin começar com campos zerados
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+        )
+    //função que altera o HTML
+    function updatedModel(e: ChangeEvent<HTMLInputElement>){
+        setUserLogin({
+// spread operator (...) espalha as informações de userLogin para dentro de setUserLogin
+            ...userLogin,
+// captura chave e valor dos campos e envia a model
+            [e.target.name]: e.target.value
+        })
+    }
+
+    useEffect(() => {
+        if(token != ''){
+            navigate('/home')
+        }
+    }, [token])
+
+// configurando o submit do login
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+// previne o comportamento padrão do botão de atualizar a tela
+        e.preventDefault();
+        try {
+            await login(`/usuarios/logar`, userLogin, setToken)
+// está pegando o token da API e jogando na localstorage para manter conectado
+            alert('Usuário logado com sucesso');
+        } catch (error) {
+            alert('Dados do usuário inconsistentes. Erro ao logar!')
+        }
+    }
+
+
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center'xs={ 6 }>
                 <Box paddingX={ 20 }>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth></TextField>
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth></TextField>
+                        <TextField value={ userLogin.usuario } onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth></TextField>
+                        <TextField value={ userLogin.senha } onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth></TextField>
                         <Box marginTop = { 2 } textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
+                          
                                 <Button type='submit' variant='contained' color='primary'>
                                     Logar
                                 </Button>
-                            </Link>
+                            
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={ 2 }>
---
+++ src/services/Service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: 'https://bloggeneration.herokuapp.com'
+})
+
+export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data.token)
+}
+
+//url irá concatenar a baseURL (linha 4) com a url de login da API (/usuarios/login)
+//dados receberá o json com as informações inseridas nos campos
+//setDado objeto irá receber os dados do usuario e o token da API
+export const login = async(url: any, dados: any, setDado: any) => {
+    //adicionando ao método post da api url e dados
+    const resposta = await api.post(url, dados)
+    //guarda os dados da variavel acima
+    setDado(resposta.data.token)
+}
