60a6173e8bf1faef63a27a11cf88fd9c701bf5ad
Jesus <je-farias@hotmail.com>
2022-10-18 21:33:32 -0300

    [Deploy Github]

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Grid, Button } from '@material-ui/core';
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import { Box } from '@mui/material';
 //importando gri e paper (background) do material ui
 import './Home.css';
@@ -23,6 +24,7 @@ function Home() {
                     <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem/>
                 </Grid>
             </Grid>
         </>
